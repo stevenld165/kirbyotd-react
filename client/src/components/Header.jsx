6c755c527e95a88be83ad21061d31d838a7411f3
Steven <stevenld165@gmail.com>
2025-01-13 03:34:04 -0600

    Added cool logo and also updated docker-compose.yml to include timezone settings

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react"
 import "./Header.scss"
 
+import logo from "../images/kirbyotd-icon.png"
+
 function Header() {
   return (
-    <div className='header'>
-      <h1>kirby of the day!!</h1>
-      <p>{new Date().toLocaleDateString()}</p>
-    </div>
+    <>
+      <div className='header'>
+        <div className='header-title'>
+          <h1>kirby of the day!!</h1>
+          <p>{new Date().toLocaleDateString()}</p>
+        </div>
+        <img src={logo} alt='logo' />
+      </div>
+    </>
   )
 }
 
